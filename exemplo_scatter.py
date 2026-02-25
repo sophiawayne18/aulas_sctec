e09680924f7e52bc4d6f28bfa14c0be4da338628
sophiawayne18 <sophialeitewayne@gmail.com>
2026-02-25 10:53:55 -0300

    scatter

---
+++ exemplo_scatter.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+# Definindo as variaveis
+
+x = [1,2,3,4,5,6,7,8]
+y = [5,2,4,5,6,8,4,8]
+
+#Criando um grafico de dispersão
+plt.scatter(x,y, label= 'Pontos', color = 'orange', marker = '*', s = 100)
+
+plt.legend()
+
+plt.show()
