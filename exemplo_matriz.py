da8bd9ae1a1040f829ef75e04094943367890674
sophiawayne18 <sophialeitewayne@gmail.com>
2026-02-25 10:53:07 -0300

    matriz

---
+++ exemplo_matriz.py
@@ -0,0 +1,54 @@
+import numpy as np  
+
+#arr2 = np.array([
+ #   [1, 2, 3],
+ #   [4, 5, 6],
+ #   [7, 8, 9]
+#])
+
+#print(arr2[2,1]) # saída esperada: 8
+
+# Informações da matriz
+#print(f"Shape da matriz:{arr2.shape}")
+#print(f'Número de elementos: {arr2.size}')
+#print(f'Tipo dos elementos: {arr2.dtype}')
+
+# Operações matematicas
+
+#arr1 = np.array([10, 20, 30, 40, 50])
+
+#print(arr1 + 10)
+
+#print(arr2 * 2)
+
+#print('Media:')
+#print(np.mean(arr1))
+
+#print ('Mediana:')
+#print(np.median(arr1))
+
+#print ("desvio padrão:")
+#desvio_padrao = np.std(arr1)
+#print(desvio_padrao)
+
+#print('Variancia')
+#variancia = np.var(arr1)
+#print(variancia)
+
+###########################
+###Tarefas###
+#############################
+
+#notas = np.array([5,8,7,3,9,7])
+
+#print(notas)
+
+#medias = np.mean(notas)
+#print(f'Médias: {medias}')
+
+
+#min = np.min(notas)
+#max = np.max(notas)
+
+#print(f"Mínimo: {min}")
+#print(f'Máximo: {max}')
